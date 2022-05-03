85897ea45905b37ca2ba9be27ab517a7d1b1ed92
hailee-moana <hailee.moana@gmail.com>
2022-05-03 18:23:28 +1000

    Added icon derived from API

--- src/app.js
+++ src/app.js
@@ -34,9 +34,15 @@ function displayTemperature(response) {
   windElement.innerHTML = response.data.wind.speed;
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "21b02ea326cb59e497753608964f41f1";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Bangkok&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Melbourne&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
